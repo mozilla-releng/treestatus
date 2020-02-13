02c9347b378d72c163dfd74174f33175fa7228e2
Rail Aliiev <rail@mozilla.com>
2020-02-12 20:33:40 -0500

    Address comments

--- frontend/build-and-copy.sh
+++ frontend/build-and-copy.sh
@@ -9,12 +9,16 @@ TO=./../api/src/treestatus_api/static/ui
 rm -f $RESULT
 rm -rf $OUT
 mkdir $OUT
+echo "Building frontend..."
 docker container rm treestatus || :
 docker run --name treestatus -v $(pwd):/src nixos/nix sh -c "cd /src && nix-build -o $RESULT"
 # pipe through tar to avoid permission denied issues
-docker container cp treestatus:$(readlink -f $RESULT)/ - | tar -C $OUT --strip-components=1 -x
-sudo chown $(id -un): -R $OUT
+docker container cp treestatus:$(readlink $RESULT)/ - | tar -C $OUT --strip-components=1 -x
 chmod 644 $OUT/*
 
 rm -f $TO/*
 cp -R $OUT/* $TO/
+rm -rf $RESULT $OUT
+
+echo "Done"
+echo "Please commit the changes located in $TO"
