e9da6969cbfd628e0b2caaba9c5ff0972b29d40a
Rail Aliiev <rail@mozilla.com>
2020-02-12 20:45:32 -0500

    Regenerate frontend in docker (#20)
    Fixes #17

--- frontend/build-and-copy.sh
+++ frontend/build-and-copy.sh
@@ -2,15 +2,23 @@
 
 set -e
 
-FROM=./result
+RESULT=./result
+OUT=./out
 TO=./../api/src/treestatus_api/static/ui
 
-rm -f $FROM
-nix-build -o $FROM
+rm -f $RESULT
+rm -rf $OUT
+mkdir $OUT
+echo "Building frontend..."
+docker container rm treestatus || :
+docker run --name treestatus -v $(pwd):/src nixos/nix sh -c "cd /src && nix-build -o $RESULT"
+# pipe through tar to avoid permission denied issues
+docker container cp treestatus:$(readlink $RESULT)/ - | tar -C $OUT --strip-components=1 -x
+chmod 644 $OUT/*
 
 rm -f $TO/*
-cp $FROM/* $TO/ -R
-sudo chown $(id -un): $TO -R
-chmod +rw $TO -R
+cp -R $OUT/* $TO/
+rm -rf $RESULT $OUT
 
-
+echo "Done"
+echo "Please commit the changes located in $TO"
